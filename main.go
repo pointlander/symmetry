417d9e39721d4fe37efe445ca947e0a553556830
Andrew Snodgrass <pointlander@gmail.com>
2026-02-03 11:06:25 -0600

    Added books

--- main.go
+++ main.go
@@ -5,15 +5,58 @@
 package main
 
 import (
+	"compress/bzip2"
 	"crypto/ed25519"
+	"embed"
 	"encoding/base32"
 	"fmt"
 	"hash/crc64"
+	"io"
 	"math"
 	"math/rand"
 
 	"golang.org/x/crypto/sha3"
 )
+
+//go:embed books/*
+var Books embed.FS
+
+// Book is a book
+type Book struct {
+	Name string
+	Text []byte
+}
+
+// LoadBooks loads books
+func LoadBooks() []Book {
+	books := []Book{
+		{Name: "pg74.txt.bz2"},
+		{Name: "10.txt.utf-8.bz2"},
+		{Name: "76.txt.utf-8.bz2"},
+		{Name: "84.txt.utf-8.bz2"},
+		{Name: "100.txt.utf-8.bz2"},
+		{Name: "1837.txt.utf-8.bz2"},
+		{Name: "2701.txt.utf-8.bz2"},
+		{Name: "3176.txt.utf-8.bz2"},
+	}
+	load := func(book string) []byte {
+		file, err := Books.Open(book)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		breader := bzip2.NewReader(file)
+		data, err := io.ReadAll(breader)
+		if err != nil {
+			panic(err)
+		}
+		return data
+	}
+	for i := range books {
+		books[i].Text = load(fmt.Sprintf("books/%s", books[i].Name))
+	}
+	return books
+}
 
 // Reader is a random number generator
 type Reader struct {
@@ -54,4 +97,6 @@ func main() {
 	onion_address = append(onion_address, version)
 	address := base32.StdEncoding.EncodeToString(onion_address) + ".onion"
 	fmt.Println(address)
+
+	LoadBooks()
 }
